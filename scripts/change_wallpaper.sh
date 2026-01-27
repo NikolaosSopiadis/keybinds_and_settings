10a8cb9853056ddac5a34cef894adea2634547c3
NikolaosSopiadis <99847949+NikolaosSopiadis@users.noreply.github.com>
2026-01-28 00:59:58 +0200

    spicetify config

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -11,6 +11,3 @@ hyprctl hyprpaper reload ,"$WALLPAPER"
 
 # Change color scheme with pywal
 wal -i "$WALLPAPER" --cols16 lighten --saturate 0.20
-
-# Change hyprland colors ##
-"$HOME/scripts/change_hyprland_colors.sh"
